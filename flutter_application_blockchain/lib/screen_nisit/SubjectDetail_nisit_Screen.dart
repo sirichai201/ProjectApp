b3ddfb4f9f01960f00829db61d6b765fec601942
sirichai201 <sirichaichantharasri4@gmail.com>
2023-09-05 12:59:15 +0700

    check in แล้ว ยังค้างอยู่ที่ เดิม

--- flutter_application_blockchain/lib/screen_nisit/SubjectDetail_nisit_Screen.dart
+++ flutter_application_blockchain/lib/screen_nisit/SubjectDetail_nisit_Screen.dart
@@ -16,14 +16,13 @@ class SubjectDetail_nisitScreen extends StatefulWidget {
       _SubjectDetail_nisitScreenState();
 }
 
-//สร้างคลาส User สำหรับเก็บข้อมูลผู้ใช้:
 class User {
   final String name;
 
   User({required this.name});
 }
 
-//สร้างคลาส Subject สำหรับเก็บข้อมูลวิชา:
+// Define the Subject class
 class Subject {
   final String name;
   final String code;
@@ -32,54 +31,87 @@ class Subject {
 }
 
 class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
-  String locationName = 'มหาวิทยาลัยเกษตรศาสตร์ สกลนคร'; // ค่าเริ่มต้น
+  String locationName = 'มหาวิทยาลัยเกษตรศาสตร์ สกลนคร';
 
-  double latitude = 13.7563; // ละติจูดตั้งต้น
-  double longitude = 100.5018; // ลองจิจูดตั้งต้น
+  double latitude = 13.7563;
+  double longitude = 100.5018;
 
   String userName = '';
   String subjectName = '';
-
-  @override
-  void initState() {
-    super.initState();
-    final user = User(name: 'ชื่อของผู้ใช้'); // เปลี่ยนเป็นข้อมูลผู้ใช้จริง
-    final subject = Subject(
-        name: 'ชื่อวิชา', code: 'รหัสวิชา'); // เปลี่ยนเป็นข้อมูลวิชาจริง
-    userName = user.name;
-    subjectName = '${subject.name} (${subject.code})';
-  }
+  String subjectCode = '';
+  String checkInDate = ''; // เพิ่มตัวแปรเก็บวันที่เช็คชื่อ
 
   Widget _buildDrawerItem({
     required String title,
     required IconData icon,
     required VoidCallback onTap,
   }) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.7,
-      margin: EdgeInsets.only(top: 8.0, bottom: 8.0, left: 8.0, right: 8.0),
-      decoration: BoxDecoration(
-        border:
-            Border.all(color: Colors.grey, width: 2), // กำหนดเส้นโครงของกรอบ
-        borderRadius: BorderRadius.circular(
-            10.0), // กำหนดขอบเส้นโครงเป็นรูปสี่เหลี่ยมเหลี่ยม
-      ),
-      child: ListTile(
-        leading: Icon(icon),
-        title: Text(title),
-        onTap: onTap,
-      ),
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: onTap,
     );
   }
 
-//สร้างเมธอดเพื่ออัปเดตชื่อตำแหน่งเมื่อมีการเปลี่ยนแปลง:
+//สร้างฟังก์ชันเพื่อแสดง DatePicker:
+  Future<void> _showDatePicker() async {
+    final selectedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2023), // กำหนดวันแรกที่สามารถเลือกได้
+      lastDate: DateTime(2030), // กำหนดวันสุดท้ายที่สามารถเลือกได้
+    );
+
+    if (selectedDate != null) {
+      // ตรวจสอบว่าผู้ใช้เลือกวันที่หรือยกเลิก
+      _setCheckInDate(selectedDate); // เรียกฟังก์ชันเพื่อเก็บวันที่เช็คชื่อ
+      setState(() {
+        // อัปเดต UI ด้วยการเรียก setState
+        checkInDate = selectedDate.toString();
+      });
+    }
+  }
+
+// สร้างฟังก์ชันเพื่อบันทึกวันที่เช็คชื่อล่าสุด
+  Future<void> _saveCheckInDate() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final currentDate = DateTime.now();
+    final formattedDate = currentDate.toString(); // รับวันที่และเวลาปัจจุบัน
+
+    await prefs.setString(
+        'checkInDate_${widget.subject['code']}', formattedDate);
+  }
+
+// ฟังก์ชันสำหรับเก็บวันที่เช็คชื่อ
+  Future<void> _setCheckInDate(DateTime selectedDate) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final formattedDate = selectedDate.toString(); // รับวันที่ที่ผู้ใช้เลือก
+
+    await prefs.setString(
+        'checkInDate_${widget.subject['code']}', formattedDate);
+    // บันทึกวันที่ที่ผู้ใช้เลือกใน SharedPreferences
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final user = User(name: 'ชื่อของผู้ใช้');
+    final subject = Subject(name: 'ชื่อวิชา', code: 'รหัสวิชา');
+    userName = user.name;
+    subjectName = widget.subject['name'] ?? 'รายละเอียดรายวิชา';
+    subjectCode = widget.subject['code'] ?? '';
+
+    _checkAndShowCheckInStatus();
+  }
+
+  // เพิ่มฟังก์ชันเพื่ออัปเดตชื่อตำแหน่งเมื่อมีการเปลี่ยนแปลง:
   void updateLocationName(String newName) {
     setState(() {
       locationName = newName;
     });
   }
 
-// ฟังก์ชันสร้าง Google Map
+  // ฟังก์ชันสร้าง Google Map
   Widget _buildGoogleMap() {
     return Column(
       children: [
@@ -117,7 +149,7 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.black), // สีขอบของกรอบ
                   borderRadius:
-                      BorderRadius.circular(8.0), // กำหนดรูปแบบของกรอบ
+                      BorderRadius.circular(8.0), // กำหนดรูปแบบขอบกรอบ
                 ),
                 child: Text(
                   locationName, // ใช้ชื่อตำแหน่งที่เรากำหนด
@@ -133,31 +165,130 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
     );
   }
 
-//สร้างฟังก์ชันเพื่อบันทึกสถานะ Check In:
-  Future<void> _saveCheckInStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(
-        'checkedIn', false); // บันทึกสถานะ Check In ว่าเกิดขึ้นแล้ว
+  void _showCheckInRequestDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('คุณต้องการ Check-In หรือไม่?'),
+          content: Text('การ Check-In จะทำให้คุณได้รับรางวัล KU COIN +0.5'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _performCheckIn();
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+              ),
+              child: Text('Check-In'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              child: Text('ยกเลิก'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
-//สร้างฟังก์ชันเพื่อตรวจสอบสถานะการ Check In และแสดงข้อความ:
+// ฟังก์ชันสำหรับบันทึกวันที่เช็คชื่อล่าสุด:
+  // ฟังก์ชันสำหรับบันทึกวันที่และเวลาเมื่อมีการ Check-In
+  Future<void> _saveLastCheckInDate() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final currentDate = DateTime.now();
+    final formattedDate = currentDate.toString(); // รับวันที่และเวลาปัจจุบัน
+
+    await prefs.setString(
+        'lastCheckInDateTime_${widget.subject['code']}', formattedDate);
+  }
+
+  void _performCheckIn() async {
+    // นี่คือส่วนของการดำเนินการ Check-In ของคุณ
+    // คุณสามารถเพิ่มโค้ดเพื่อบันทึกสถานะ Check-In หรือดำเนินการอื่น ๆ ตามต้องการ
+
+    _saveCheckInStatus(); // บันทึกสถานะ Check-In
+    _saveLastCheckInDate(); // บันทึกวันที่เช็คชื่อล่าสุด
+
+    // เพิ่มบรรทัดนี้เพื่อบันทึกวันที่เช็คชื่อล่าสุด
+    _saveCheckInDate();
+
+    // แสดง Dialog การยืนยัน Check-In
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('คุณได้ Check-In แล้ว'),
+          content: Text('คุณได้รับรางวัล KU COIN +0.5'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // ปิด Dialog นี้
+                Navigator.of(context).pop(); // ปิดหน้าจอการ Check-In
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+              ),
+              child: Text('ตกลง'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //สร้างฟังก์ชันเพื่อบันทึกสถานะ Check In:
+  Future<void> _saveCheckInStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('checkedIn_${widget.subject['code']}',
+        true); // บันทึกสถานะ Check-In เป็น true
+  }
+
+  //สร้างฟังก์ชันเพื่อตรวจสอบสถานะการ Check In และแสดงข้อความ:
+  //ใน _checkAndShowCheckInStatus() เพิ่มเงื่อนไขเพื่อตรวจสอบว่าได้ Check-In ในวันเดียวกันหรือไม่:
+  // ฟังก์ชันเพื่อตรวจสอบสถานะการ Check-In และแสดงข้อความ:
+  // แก้ไขและเพิ่มเงื่อนไขตรวจสอบสถานะ Check-In ใน _checkAndShowCheckInStatus
   Future<void> _checkAndShowCheckInStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool checkedIn = prefs.getBool('checkedIn') ??
-        false; // อ่านสถานะ Check In และตั้งค่าเริ่มต้นเป็น false ถ้ายังไม่มีค่า
+    bool checkedIn =
+        prefs.getBool('checkedIn_${widget.subject['code']}') ?? false;
+    String lastCheckInDateTime =
+        prefs.getString('lastCheckInDateTime_${widget.subject['code']}') ??
+            'ยังไม่เคย Check-In';
+    String checkInDate =
+        prefs.getString('checkInDate_${widget.subject['code']}') ??
+            'ยังไม่ได้กำหนดวันที่เช็คชื่อ';
+
+    setState(() {
+      this.checkInDate = checkInDate; // อัปเดตค่า checkInDate ใน State
+    });
 
     if (checkedIn) {
-      // ignore: use_build_context_synchronously
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
             title: Text('คุณได้ Check In ไปแล้ว'),
-            content: Text('คุณไม่สามารถ Check In ซ้ำได้อีก'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('คุณไม่สามารถ Check In ซ้ำได้อีก'),
+                Text(
+                    'วันที่และเวลาของการ Check-In ล่าสุด: $lastCheckInDateTime'),
+                Text('วันที่ที่เช็คชื่อ: $checkInDate'),
+              ],
+            ),
             actions: [
               ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).pop(); // ปิด dialog
+                  Navigator.of(context).pop();
                 },
                 style: ElevatedButton.styleFrom(
                   primary: Colors.green,
@@ -169,7 +300,7 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
         },
       );
     } else {
-      _showCheckInDialog(); // แสดงหน้าต่าง Check In ปกติ
+      _showCheckInDialog();
     }
   }
 
@@ -189,7 +320,7 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
                 style: TextStyle(fontSize: 16),
               ),
               Text(
-                'วิชา: ${widget.subject['name']} (${widget.subject['code']})',
+                'วิชา: $subjectName ($subjectCode)',
                 style: TextStyle(fontSize: 16),
               ),
               SizedBox(height: 10),
@@ -214,7 +345,7 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).pop(); // ปิด AlertDialog นี้
-                _checkAndShowCheckInStatus(); // เรียกตรวจสอบและแสดงสถานะการ Check In อีกครั้ง
+                _showCheckInRequestDialog(); // เรียกตรวจสอบและแสดงสถานะการ Check In อีกครั้ง
               },
               style: ElevatedButton.styleFrom(
                 primary: Colors.green,
@@ -232,8 +363,7 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '${widget.subject['name']} (${widget.subject['code']})' ??
-              'รายละเอียดรายวิชา',
+          '$subjectName ($subjectCode)' ?? 'รายละเอียดรายวิชา',
         ),
       ),
       drawer: Drawer(
@@ -282,8 +412,6 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
                             subject: {},
                           )),
                 );
-
-                // เพิ่มโค้ดที่คุณต้องการเมื่อคลิกที่เมนู 'ประวัติการเข้าเรียน'
               },
             ),
             SizedBox(
@@ -296,13 +424,12 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => LoginScreen()),
-                ); // ปิด Drawer เมื่อกดปุ่ม "ออก"
+                );
               },
             ),
             SizedBox(
               height: 20,
             ),
-            // Add more ListTile items as needed
           ],
         ),
       ),
@@ -313,17 +440,14 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                _buildGoogleMap(), // เรียกใช้งาน Google Map ที่สร้างขึ้น
-
-                //SizedBox(height: 20),
-
+                _buildGoogleMap(),
                 const SizedBox(height: 30),
                 const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 20),
                   child: Divider(
-                    thickness: 1, // ความหนาของเส้น Divider
-                    color: Color.fromARGB(255, 22, 22, 22), // สีของเส้น Divider
-                    height: 10, // ระยะห่างระหว่าง SizedBox กับ PieChartWidget
+                    thickness: 1,
+                    color: Color.fromARGB(255, 22, 22, 22),
+                    height: 10,
                   ),
                 ),
                 SizedBox(height: 20),
@@ -336,12 +460,30 @@ class _SubjectDetail_nisitScreenState extends State<SubjectDetail_nisitScreen> {
                   ),
                   child: ElevatedButton(
                     onPressed: () {
-                      _showCheckInDialog(); // เรียกฟังก์ชันเมื่อกดปุ่ม "Check In"
+                      _checkAndShowCheckInStatus();
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Colors.green,
                     ),
                     child: Text('Check In'),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    _showDatePicker();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                  ),
+                  child: Text('กำหนดวันที่เช็คชื่อ'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'วันที่เช็คชื่อล่าสุด: $checkInDate',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ],
